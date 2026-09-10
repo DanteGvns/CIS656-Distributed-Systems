091626acb707b13fbe5b0807cf5f40c094c5f0e7
DanteGvns <Dante.a.g.hi@gmail.com>
2026-09-10 11:14:46 -0400

    SeverApp ready to test

--- ClientApp.java
+++ ClientApp.java
@@ -1,8 +1,17 @@
+import java.io.IOException;
+import java.net.Socket;
 import java.util.Date;
 
 public class ClientApp {
     public static void main(String[] args){
         String dateTime = new Date().toString();
         System.out.println(dateTime);
+        String serverAddress = "1.0.1";
+        int portNum = 53245;
+        try {
+            Socket socket2 = new Socket(serverAddress, portNum);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- ServerApp.java
+++ ServerApp.java
@@ -1,15 +1,30 @@
 import java.io.IOException;
 import java.net.ServerSocket;
-
-import javax.imageio.IIOException;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ServerApp {
     public static void main(String[] args){
         String startUpMessage = "This Sever is Running";
         System.out.println(startUpMessage);
-
+        int portNum = 53245;
         try { 
-            ServerSocket listener = new ServerSocket(0);
+            ServerSocket listener = new ServerSocket(portNum);
+            while (true) {
+                Socket socket1 = listener.accept();
+                System.out.println("Client connected!");
+
+                BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket1.getInputStream())
+                );
+
+                //read buffer
+                String message;
+                while ((message = in.readLine()) != null ) {
+                    System.out.println("Client says: " + message);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
