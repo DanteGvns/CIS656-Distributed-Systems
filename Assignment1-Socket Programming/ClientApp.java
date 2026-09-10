1183e85c6b69d5976ffbafa11810990c9ec92132
DanteGvns <Dante.a.g.hi@gmail.com>
2026-09-10 19:05:44 -0400

    Client now asks for the Sever IP

--- Assignment1-Socket Programming/ClientApp.java
+++ Assignment1-Socket Programming/ClientApp.java
@@ -2,13 +2,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Date;
+import java.util.Scanner;
 
 public class ClientApp {
     public static void main(String[] args){
         //set address of sever and port number to connect to
-        String serverAddress = "";
         int portNum = 53245;
+        Scanner scanner = new Scanner((System.in));
 
+        //get IP address from user
+        String prompt = "Please enter the Severs IP address:";
+        System.out.println(prompt);
+        String serverAddress = scanner.nextLine();
+
+        //connect to the sever and send message
         try {
             Socket severSocket = new Socket(serverAddress, portNum);
             String dateMessage = new Date().toString();
@@ -17,6 +24,8 @@ public class ClientApp {
             //Write the string to the output port
             PrintWriter outputSocket = new PrintWriter(severSocket.getOutputStream(), true);
             outputSocket.println(dateMessage);
+
+            //let users now the message was sent
             System.out.println(notify);
 
             //close the socket
