e4080e81d6e26368127b53f4eff0935b3f020eb6
DanteGvns <Dante.a.g.hi@gmail.com>
2026-09-17 10:29:00 -0400

    modified sever app for assignment 2

--- MultiThreadSocketProgramming-Assignment2/ServerApp.java
+++ MultiThreadSocketProgramming-Assignment2/ServerApp.java
@@ -13,35 +13,51 @@ public class ServerApp {
         
         String startUpMessage = "This Sever is Running";
         String waiting = "Waiting for another message....";
-        String sentMessage = "The date has been sent to the Client!";
-        String clientConnected = "Client connected!";
+        String sentMessage = "The message has been sent to the Client!";
+        String clientConnected = "Hello, you are client #";
+        Boolean stillRunning = true;
+        int countClients = 0;
         String dateMessage;
         String message;
+        String upperCase;
 
         System.out.println(startUpMessage);
 
         try { 
-            ServerSocket listener = new ServerSocket(portNum);
-
+            //alawys listen for a new client
             while (true) {
+                ServerSocket listener = new ServerSocket(portNum);
                 //connect to client
-                Socket appSocket = listener.accept();
-                System.out.println(clientConnected);
+                while (stillRunning) {
+                    Socket appSocket = listener.accept();
+                    System.out.println(clientConnected);
 
-                //Write the date to the socket output
-                dateMessage = new Date().toString();
-                PrintWriter outputSocket = new PrintWriter(appSocket.getOutputStream(), true);
-                outputSocket.println(dateMessage);
-                System.out.println(sentMessage);
+                    //Send out which Client they are
+                    PrintWriter outputSocket = new PrintWriter(appSocket.getOutputStream(), true);
+                    outputSocket.println(clientConnected + countClients++);
+                    System.out.println(sentMessage);
 
-                //wait for response from client
-                BufferedReader in = new BufferedReader(
-                    new InputStreamReader(appSocket.getInputStream())
-                );
-                while ((message = in.readLine()) != null ) {
-                    System.out.println(message);
-                    System.out.println(waiting);
+                    //wait for messsage from client
+                    BufferedReader in = new BufferedReader(
+                        new InputStreamReader(appSocket.getInputStream())
+                    );
+                    while ((message = in.readLine()) != null ) {
+                        if (message.trim() == "time") {
+                            System.out.println("message was time sending back time");
+                            dateMessage = new Date().toString();
+                            outputSocket.println(dateMessage);
+                        } else if (message != "") {
+                            upperCase = message.toUpperCase();
+                            System.out.println("message was " + message + " sending back time" + upperCase);
+                            outputSocket.println(upperCase);
+                        } else {
+                            appSocket.close();
+                            listener.close();
+                            stillRunning = false;
+                        }
+                    }
                 }
+                stillRunning = true;
             }
         } catch (IOException e) {
             e.printStackTrace();
